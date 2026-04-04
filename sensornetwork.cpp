83ab5ce702cb46af0200d6ad78369c0daed33f9b
PhongTrinh896 <thephongtrinh0809@gmail.com>
2026-04-05 00:47:30 +0700

    Latest ver (fixed theo ý vân anh)

--- sensornetwork.cpp
+++ sensornetwork.cpp
@@ -8,139 +8,175 @@
 #define MAX_SENSORS 30
 #define BUFFER_SIZE 10
 #define NUMBER_OF_STATS 3
+#define MAXIMUM_ERROR 20
 
 enum condition{
 	DISCONNECTED = 0,
 	CONNECTED = 1
 };
+
+enum sensor_type{
+	TEMP,
+	HUMID,
+	GAS,
+	LIGHT
+};
 typedef struct Sensor_stats{
 	uint8_t ID;
 	char sensor_name[15];
+	enum sensor_type type;
 	uint16_t frequency;
 	
 	float data, maximum_value;
 	enum condition current_state;
 	char data_file_name[12];
-	Sensor_stats *next;
 	
-	uint16_t over_counter, error_counter, overflow_counter;
+	uint16_t over_counter, error_counter, valid;
+	long overflow_counter;
 	
 	float buffer[BUFFER_SIZE];
 	uint8_t head, buffer_count;
+	
+	float max, min;
 } Sensor_stats;
 
 // Cau truc FILE sensor: ID Ten_sensor Frequency(in Hz) Max_value
-
+void calculate_max_min(Sensor_stats* S, float data);
 float receive_data_sensor(Sensor_stats *S, FILE* reportfptr);
-void add_data_to_buffer (Sensor_stats* S, float data, FILE* errorfptr);
-void add_Node(Sensor_stats *S1, Sensor_stats *S2);
-void delete_Node(Sensor_stats *S1, Sensor_stats *S2, Sensor_stats *S3);
-Sensor_stats* init_sensor(uint8_t sensor_counter, FILE* fptr);
-void update_data_to_file(Sensor_stats *S, float data);
-void check_invalid_data (float data, Sensor_stats* S, FILE* errorfptr);
-void apply_average_filter (Sensor_stats* S,float data, FILE* reportfptr);
 void send_actuator (Sensor_stats* S);
+void add_data_to_buffer (Sensor_stats* S, float data, FILE* errorfptr);
+
+Sensor_stats* init_sensor(uint8_t sensor_counter, FILE* fptr);
+void add_Node(Sensor_stats *collection[], uint8_t sensor_counter, uint8_t* total_sensor, FILE* sensorfptr);
+void delete_Node(Sensor_stats *collection[], uint8_t *total_sensor, int position, FILE* reportfptr);
+
+void update_data_to_file(Sensor_stats *S, float data);
+uint8_t check_invalid_data (float data, Sensor_stats* S, FILE* errorfptr);
+void apply_average_filter (Sensor_stats* S, float data, FILE* errorfptr);
+
+void most_error(Sensor_stats* collection[], uint8_t* total_sensor, FILE* errorfptr);
 void report(Sensor_stats* S, FILE* reportfptr);
 
 // ---------- MAIN FUNCTION -----------
 int main(){
-	time_t now = time(NULL);
-	uint8_t sensor_counter = 0;
-	FILE *sensorFileptr = fopen("SENSOR_FILE.txt", "rt");
-	if (sensorFileptr == NULL) {
-        printf("Khong the mo tep cau hinh SENSOR_FILE. Vui long kiem tra lai.\n");
+    uint8_t sensor_counter = 0, total_sensor = 0;
+
+    FILE *sensorFileptr = fopen("SENSOR_FILE.txt", "rt");
+    if (sensorFileptr == NULL) {
+        printf("Khong the mo file sensor\n");
         return 0;
     }
-	FILE *errorFileptr = fopen("ERROR_FILE.txt", "w+");
-	
-	Sensor_stats* collection[30];
-	//Sensor_stats *S1 = init_sensor(sensor_counter, sensorFileptr);
-	while (!feof(sensorFileptr) && sensor_counter < 30){
-		// Tim cach khai bao array chua nhieu sensor theo file de noi cac nodes
-		collection[sensor_counter] = init_sensor(sensor_counter, sensorFileptr);
-    	sensor_counter++;
-    	if (sensor_counter >0){
-    		add_Node(collection[sensor_counter - 1], collection[sensor_counter]);
-		}
-	}
-	fclose(sensorFileptr);
-	
-	while(1){
-		float input = receive_data_sensor(collection[0], errorFileptr);
-		if(input){
-		}
-		
-	}
-	
-	/*Sensor_stats *S2 = (Sensor_stats*)malloc(sizeof(Sensor_stats));
-	Sensor_stats *S3 = (Sensor_stats*)malloc(sizeof(Sensor_stats));
-	add_Node(S1, S2);
-	add_Node(S2, S3);
-	/*printf("Address of S2 (linked to S1): %p\n", (void *)S1->next);
-	printf("Real address of S2: %p\n", (void *)S2);
-	
-	delete_Node(S1, S2, S3);
-	/*printf("S1 now point to: %p\n", (void*)S1->next);
-	printf("Real address of S3: %p\n", (void *)S3);
-	printf("Real address of S2: %p\n", (void *)S2);*/
-	FILE *finalFileptr = fopen("REPORT_FILE.txt", "a+");
-	fseek(finalFileptr, 0, SEEK_END);
-	fprintf(finalFileptr, "FINAL REPORT ---\n");
-	report(collection[0], finalFileptr);
-	
-	
-	fclose(errorFileptr);
-	fclose(finalFileptr);
+    FILE *errorFileptr = fopen("ERROR_FILE.txt", "w+");
+    Sensor_stats* collection[MAX_SENSORS];
+
+    // ===== LOAD SENSOR =====
+    while (sensor_counter < MAX_SENSORS) {
+        Sensor_stats *S = init_sensor(sensor_counter, sensorFileptr);
+        if (S == NULL) break;
+
+        collection[sensor_counter] = S;
+        sensor_counter++;
+        total_sensor++;
+    }
+    fclose(sensorFileptr);
+    printf("Loaded %d sensors\n", total_sensor);
+
+    // ===== MAIN LOOP =====
+    while (1){
+        for (uint8_t i = 0; i < total_sensor; i++){
+
+            float input = receive_data_sensor(collection[i], errorFileptr);
+            if (input == -1000) continue;
+            
+            add_data_to_buffer(collection[i], input, errorFileptr);
+            if (check_invalid_data(input, collection[i], errorFileptr)){
+            	apply_average_filter(collection[i], input, errorFileptr);
+			}
+            if (collection[i]->error_counter > MAXIMUM_ERROR || collection[i]->current_state == DISCONNECTED){
+                delete_Node(collection, &total_sensor, i, errorFileptr);
+                i--; 
+            }
+        }
+    }
+
+    // ===== REPORT  =====
+    FILE *finalFileptr = fopen("REPORT_FILE.txt", "a+");
+    fprintf(finalFileptr, "FINAL REPORT ---\n");
+    long total_error = 0, valid_counter = 0;
+    for (int i = 0; i < total_sensor; i++){
+    	total_error += collection[i]->error_counter;
+    	valid_counter += collection[i]->valid;
+        report(collection[i], finalFileptr);
+    }
+    most_error(collection, &total_sensor, errorFileptr);
+    fprintf(finalFileptr, "\nTotal error: %ld\nTotal valid: %ld\n", total_error, valid_counter);
+    fclose(errorFileptr);
+    fclose(finalFileptr);
+    return 0;
 }
 
 //-------- Ham khoi tao Sensor ----------
-Sensor_stats* init_sensor(uint8_t sensor_counter, FILE *fptr){
-	fseek(fptr, 0, SEEK_SET);
+Sensor_stats* init_sensor(uint8_t sensor_counter, FILE *sensorfptr){
+	fseek(sensorfptr, 0, SEEK_CUR);
 	Sensor_stats* S = (Sensor_stats*)malloc(sizeof(Sensor_stats));
 	S->ID = sensor_counter;
-	uint8_t list_counter = 0;
-	do {
-		fseek(fptr, 0, SEEK_CUR);
-		fscanf(fptr, "%hu", &list_counter);
-		if (S->ID != list_counter){
-			char s[100];
-			fgets(s, 100, fptr);
-		}
-	} while (S->ID != list_counter);
-	
+	uint8_t verifier = fscanf(sensorfptr, " %s %hu %f", S->sensor_name, &S->frequency, &S->maximum_value);
+	if (verifier != NUMBER_OF_STATS)    return NULL;
 	S->current_state = CONNECTED;
 	S->error_counter = 0;
 	S->over_counter = 0;
 	S->overflow_counter = 0;
-	S->next = NULL;
+	S->valid = 0;
 	
 	S->buffer_count = 0;
 	S->head = 0;
-	S->buffer = {0};
+	memset(S->buffer, 0, sizeof(S->buffer));
 	
-	uint8_t verifier = fscanf(fptr, " %s %hu %f", S->sensor_name, S->frequency, S->maximum_value);
-	if (verifier != NUMBER_OF_STATS)    return NULL;
+	S->max = 0;
+	S->min = S->maximum_value;
 	snprintf(S->data_file_name, sizeof(S->data_file_name),"Sens%d.dat", S->ID); // tao 1 file cho tung sensor de ghi data
 	return S;
 }
 
 // -------- Cac ham thao tac Nodes ------
-void add_Node(Sensor_stats *S1, Sensor_stats *S2){
-	S1->next = S2; 
+void add_Node(Sensor_stats *collection[], uint8_t sensor_counter, uint8_t* total_sensor, FILE* sensorfptr){
+	if (sensor_counter == MAX_SENSORS){
+		printf("MAXIMUM NUMBER OF SENSOR REACHED");
+		return;
+	}
+	rewind(sensorfptr);
+	for (uint8_t i = 0; i < sensor_counter; i++){
+		char s[100];
+		fgets(s, sizeof(s), sensorfptr);
+	}
+	collection[sensor_counter] = init_sensor(sensor_counter + 1, sensorfptr);
+	*total_sensor ++;
 }
 
-void delete_Node(Sensor_stats *S1, Sensor_stats *S2, Sensor_stats *S3, FILE* reportfptr){
-	S1->next = S3;
-	time_t now = time(NULL);
-	fprintf(reportfptr, "Sensor ID %hu named %s disconnected at %s", S2->ID, S2->sensor_name, ctime(&now));
-	free(S2);
+void delete_Node(Sensor_stats *collection[], uint8_t *total_sensor, int position, FILE* reportfptr){
+    if (position < 0 || position >= *total_sensor) return;
+    time_t now = time(NULL);
+    fprintf(reportfptr,
+        "Sensor ID %hu named %s disconnected at %s",
+        collection[position]->ID,
+        collection[position]->sensor_name,
+        ctime(&now));
+	report(collection[position], reportfptr);
+    free(collection[position]);
+    for (int i = position; i < *total_sensor - 1; i++){
+        collection[i] = collection[i + 1];
+    }
+
+    (*total_sensor)--;
 }
+
 
 //------- Cac ham thao tac du lieu -------
 // Update data vao trong file ghi du lieu rieng tung sensor
 void update_data_to_file(Sensor_stats *S, float data){ 
+	calculate_max_min(S, data);
 	FILE *fptr = fopen(S->data_file_name, "a+t");
-	fprintf(fptr, "%hu\n", data);
+	fprintf(fptr, "%f\n", data);
 	fclose(fptr);
 }
 
@@ -169,6 +205,7 @@ uint8_t check_invalid_data (float data, Sensor_stats* S, FILE* errorfptr){
 		apply_average_filter(S, data, errorfptr);
 		return 1;
 	}
+	update_data_to_file(S, data);
 	return 0;
 }
 
@@ -193,36 +230,63 @@ void send_actuator (Sensor_stats* S){
 
 //Sensor nhan input theo chu ky nhan tin hieu
 float receive_data_sensor(Sensor_stats *S, FILE *reportfptr){
-	float cycle_timer = 1/(S->frequency);
+	double cycle_timer = 1/(S->frequency);
 	Sleep(cycle_timer*1000);
 	float input;
+	S->valid++;
 	if (scanf("%f", &input)){
 		return input;
 	}
 	else {
 		fprintf(reportfptr, "Ivalid input to sensor ID %hu", S->ID);
 		S->error_counter ++;
-		return 0;
+		return -1000;
 	}
 }
 
 // --------- Cac ham ghi report -----------
+void calculate_max_min(Sensor_stats* S, float data){
+	if (S->max < data)   { S->max = data; }
+	if (S->min > data)   { S->min = data; }
+	return;
+}
+
+void most_error(Sensor_stats* collection[], uint8_t* total_sensor, FILE* errorfptr){
+	fprintf(errorfptr, "\n----- Most error -----\n");
+	int most_error = 0;
+	for (uint8_t i = 0; i < *total_sensor; i++){
+		if (most_error < collection[i]->error_counter){
+			most_error = collection[i]->error_counter;
+		}
+	}
+	for (uint8_t i = 0; i < *total_sensor; i++){
+		if (most_error == collection[i]->error_counter){
+			fprintf(errorfptr, "Sensor with the most error: %s, ID: %hu\n", collection[i]->sensor_name, collection[i]->ID);
+		}
+	}
+}
 
 void report(Sensor_stats* S, FILE* reportfptr){
 	FILE *sensorfptr = fopen(S->data_file_name, "r");
 	rewind(sensorfptr);
 	uint16_t counter = 0;
-	double summing_data;
-	float max = 0, min= S->maximum_value;
-	while (!feof(sensorfptr)){
-		float x;
+	double summing_data = 0;
+	float x;
+	while (fscanf(sensorfptr, "%f", &x) == 1){
 		fscanf(sensorfptr, "%f", &x);
 		summing_data += x;
-		if (max < x)   { max = x; }
-		if (min > x)   { min = x; }
 		counter++;
 	}
 	float avg_value = summing_data/counter;
-	fprintf(reportfptr, "Sensor ID %hu:\n  So luong ban tin loi: %hu\n  So lan vuot nguong: %hu\n  GTLN/GTNN/GTTB: %f/%f/%f\n", S->error_counter, S->over_counter, max, min, avg_value);
+	fprintf(reportfptr, "\n------------\n");
+	fprintf(reportfptr, "Sensor ID %hu:\n  So luong ban tin loi: %hu\n  So lan vuot nguong: %hu\n  So lan tran bo dem:%ld\n  GTLN/GTNN/GTTB: %f/%f/%f\n", S->error_counter, S->over_counter, S->overflow_counter, S->max, S->min, avg_value);
+	fprintf(reportfptr, "\n------------\n");
 	fclose(sensorfptr);
 }
+
+/***NOTE:
+	- Sensor type
+	- Report theo từng type sensor
+	- Xử lý mất mẫu
+	- Check ChatGPT
+*/
